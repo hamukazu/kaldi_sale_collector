a1c5f0d9150184d5c88e65c4c30e4d3081449ede
Kimikazu Kato <kimikazu@gmail.com>
2024-01-31 00:40:50 +0900

    Active sales are highlighted

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ def split(s):
         return (s, "")
 
 
+def parse_date(s):
+    a = s.split("～")
+    ret = []
+    for x in a:
+        dt = datetime.strptime(re.sub("\(.*\)", "", x.strip()), "%Y年%m月%d日")
+        ret.append(dt.isoformat())
+    return ret
+
+
+def include_now(now, strdate_from, strdate_to):
+    datetime_from = datetime.fromisoformat(strdate_from + "+09:00")
+    datetime_to = datetime.fromisoformat(strdate_to + "+09:00") + timedelta(hours=21)
+    return now >= datetime_from and now <= datetime_to
+
+
 # itertools.batched is implemented in Python 3.12
 # This function is for earlier version
 def batched(l, n):
@@ -28,9 +43,9 @@ def batched(l, n):
     return ret
 
 
-def get_prefectures():
+def get_prefectures(home_dir="."):
     prefectures = []
-    with open("japan_prefectures/japan_prefectures.csv") as fp:
+    with open(f"{home_dir}/japan_prefectures/japan_prefectures.csv") as fp:
         reader = csv.reader(fp)
         next(reader)
         for row in reader:
@@ -53,6 +68,9 @@ def get_pref_sale(prefectures, shops, sale):
         shop, shop_note = split(s["shop"])
         s["shop"] = shop
         s["shop_note"] = shop_note
+        date_from, date_to = parse_date(s["date"])
+        s["date_from"] = date_from
+        s["date_to"] = date_to
         pref = d.get(s["shop"], "NOT_FOUND")
         pref_sale[pref].append(s)
     if len(pref_sale["NOT_FOUND"]) > 0:
@@ -61,14 +79,24 @@ def get_pref_sale(prefectures, shops, sale):
     return pref_sale
 
 
-def write(prefectures, pref_sale, fp):
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("./"))
+def write(prefectures, pref_sale, fp, home_dir="."):
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(home_dir + "/"))
     template = env.get_template("template.html")
     tz_jst = timezone(timedelta(hours=9), name="JST")
-    datestr = datetime.strftime(datetime.now(tz=tz_jst), "%Y-%m-%d %H:%H:%M")
+    now = datetime.now(tz=tz_jst)
+    datestr = datetime.strftime(now, "%Y-%m-%d %H:%H:%M")
+    pref_sale_modified = {}
+    for k in pref_sale:
+        ss = []
+        for d in pref_sale[k]:
+            dd = d.copy()
+            dd["include_now"] = include_now(now, dd["date_from"], dd["date_to"])
+            ss.append(dd)
+        pref_sale_modified[k] = ss
+
     html = template.render(
         prefectures=prefectures,
-        pref_sale=pref_sale,
+        pref_sale=pref_sale_modified,
         datestr=datestr,
         batched=batched,
     )
@@ -82,6 +110,8 @@ def main():
         sale = json.load(fp)
     prefectures = get_prefectures()
     pref_sale = get_pref_sale(prefectures, shops, sale)
+    with open("pref_sale.json", "w") as fp:
+        json.dump(pref_sale, fp)
     with open("index.html", "w") as fp:
         write(prefectures, pref_sale, fp)
 
